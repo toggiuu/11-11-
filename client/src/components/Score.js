a7b3eee7f858f9af3c2a66af46f0f3a115173335
toggiuu <106525817+toggiuu@users.noreply.github.com>
2024-05-28 19:17:48 +0900

    Update Score.js

--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -1,11 +1,13 @@
+// Score.js
 import React from 'react';
+import './Score.css';
 
 const Score = ({ score }) => {
-  return (
-    <div className="score">
-      <h1>Score: {score}</h1>
-    </div>
-  );
+    return (
+        <div className="score">
+            <h2>Score: {score}</h2>
+        </div>
+    );
 };
 
 export default Score;
